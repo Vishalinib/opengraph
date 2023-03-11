182b547e60e83841c9ada8b7bbcffcc9b4b8bfcf
Vishalinib <127511718+Vishalinib@users.noreply.github.com>
2023-03-11 10:34:11 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Open Graph | Home</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
+    <meta property="og:title" content="Open Graph Demo" />
+    <meta property="og:type" content="website" />
+    <meta property="og:description" content="An Open Graph Demo " />
+    <meta property="og:url" content="http://localhost/opengraph/index"/>
+    <meta property="og:image" content="C:\Users\vishalini\Documents\opengraph\assets\images\images.jfif" />
+</head>
+<body>
+    <header class="site-header">
+        <div class="site-identity">
+          <a href="#">
+            <img src="C:\Users\vishalini\Documents\opengraph\assets\images\th.jfif" alt="Site Name" /></a>
+          <h1><a href="#">Open Graph</a></h1>
+        </div>  
+        <nav class="site-navigation">
+          <ul class="nav">
+            <li><a href="#">Home</a></li> 
+            <li><a href="#">About</a></li> 
+            <li><a href="#">Contact</a></li> 
+            <a href="https://www.facebook.com/sharer/sharer.php?u=http://localhost/opengraph/index" class="fa fa-facebook"></a>
+          </ul>
+        </nav>
+      </header>
+
+    <h1>Digital Marketing</h1>
+</body>
+</html>
